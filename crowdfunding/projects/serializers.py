bf414a0480f2174d25c7086c4d66dc909b7ac529
jdub-codes <jenny.waller@riotinto.com>
2023-07-29 13:04:11 +1000

    model relationships set-up

--- crowdfunding/projects/serializers.py
+++ crowdfunding/projects/serializers.py
@@ -1,6 +1,11 @@
 from rest_framework import serializers
 from django.apps import apps
 
+class PledgeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = apps.get_model('projects.Pledge')
+        fields = '__all__'
+
 class ProjectSerializer(serializers.ModelSerializer):
     class Meta:        
         model = apps.get_model('projects.Project')
