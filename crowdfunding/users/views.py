58e2855268f9498094178ae9c1ecb755661c9e6c
jdub-codes <jenny.waller@riotinto.com>
2023-09-17 22:26:41 +1000

    Updated views for users

--- crowdfunding/users/views.py
+++ crowdfunding/users/views.py
@@ -1,7 +1,7 @@
 from django.http import Http404
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import status, permissions
 
 from .permissions import IsOwnerOrAdmin
 from rest_framework.permissions import IsAdminUser
